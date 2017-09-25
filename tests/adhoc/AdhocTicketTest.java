6c4eba23a661cd39c3814c5cff8efd811620961a
Nimendra Madampe <nimendramadampe93@gmail.com>
2017-09-25 08:39:10 +0530

    Added Test Case for getTicketNo method

--- tests/adhoc/AdhocTicketTest.java
+++ tests/adhoc/AdhocTicketTest.java
@@ -68,12 +68,10 @@ public class AdhocTicketTest {
     @Test
     public void testGetTicketNo() {
         System.out.println("getTicketNo");
-        AdhocTicket instance = null;
+        AdhocTicket instance = new AdhocTicket("CARP00001", 1, "BS19951025");
         int expResult = 0;
         int result = instance.getTicketNo();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
