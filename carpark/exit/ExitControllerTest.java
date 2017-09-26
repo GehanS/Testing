55a0964438f2429a689a9b98c53493e12f391f8b
longdd1997 <30396806+longdd1997@users.noreply.github.com>
2017-09-26 15:18:13 +1000

    Update ExitControllerTest.java

--- carpark/exit/ExitControllerTest.java
+++ carpark/exit/ExitControllerTest.java
@@ -1,29 +1,4 @@
 
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-		
-    }
-
-	 /**
-     * Unit Testing of carEventDetected method, of class ExitController.
-     */
-	 
-	  @Test
-    public void testCarEventDetected() {
-        System.out.println("carEventDetected");
-        String detectorId = "";
-		boolean carDetected = false;
-        ExitController instance = null;
-        instance.carEventDetected(detectorId, carDetected);
-        // TODO review the generated test code and remove the default call to fail.
-        
     }
 
 	/**
